ea04eab155bff9056473e9abbcbe391d2e171748
SpaceT1 <147409033+SpaceT1@users.noreply.github.com>
2023-10-13 13:25:20 +0600

    Delete ComputerFactory.java

--- ComputerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.journaldev.design.abstractfactory;
-import com.journaldev.design.model.Computer;
-public class ComputerFactory {
-    public static Computer getComputer(ComputerAbstractFactory factory){
-        return factory.createComputer();
-    }
-}
-/// фабрика для использования введеных пользователем данных и перевод их в нужную фабрику с последующим созданием пк
