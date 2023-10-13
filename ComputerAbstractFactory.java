33a15168222bd6ea32b420eacde47ccd49aac529
SpaceT1 <147409033+SpaceT1@users.noreply.github.com>
2023-10-13 13:25:11 +0600

    Delete ComputerAbstractFactory.java

--- ComputerAbstractFactory.java
+++
@@ -1,7 +0,0 @@
-package com.journaldev.design.abstractfactory;
-import com.journaldev.design.model.Computer;
-public interface ComputerAbstractFactory {
-    public Computer createComputer();
-}
-
-///класс для распространение метода createComputer с класса Cmoputer на другие фабрики (PC and Server)
